b28380020355e1f045fda84fa4ab672db1e004bd
ryanpbrewster <RyanPBrewster@gmail.com>
2013-05-29 17:20:12 -0500

    Added a rollBy function to Util.hs.

--- ProjectEuler/Util.hs
+++ ProjectEuler/Util.hs
@@ -1,5 +1,6 @@
 module ProjectEuler.Util
 ( roll
+, rollBy
 , tuples
 ) where
 
@@ -7,6 +8,11 @@ module ProjectEuler.Util
 roll _ [] = []
 roll k xs = let (front,back) = splitAt k xs
             in front:roll k back
+
+-- rollBy generalizes roll in that it allows a list of ks instead of a number
+rollBy _ [] = []
+rollBy (k:ks) xs = let (front,back) = splitAt k xs
+                   in front:rollBy ks back
 
 
 -- tuples 2 [1,2,3] == [ [1,1], [1,2], [1,3], [2,1], [2,2], [2,3], [3,1], [3,2], [3,3] ]
