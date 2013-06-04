b8a9b31bbbaddcf785c5f7d7a4c3ea46a8bc39c2
ryanpbrewster <RyanPBrewster@gmail.com>
2013-06-04 13:38:24 -0500

    Added a function that merges an infinite list of infinite lists.

--- ProjectEuler/Util.hs
+++ ProjectEuler/Util.hs
@@ -2,6 +2,7 @@ module ProjectEuler.Util
 ( roll
 , rollBy
 , tuples
+, mergeInf
 ) where
 
 -- roll 3 [1..] == [ [1,2,3], [4,5,6], [7,8,9], ... ]
@@ -18,3 +19,10 @@ rollBy (k:ks) xs = let (front,back) = splitAt k xs
 -- tuples 2 [1,2,3] == [ [1,1], [1,2], [1,3], [2,1], [2,2], [2,3], [3,1], [3,2], [3,3] ]
 tuples 0 _ = [[]] -- only the empty tuple
 tuples k xs = [ x:t | t <- tuples (k-1) xs, x <- xs ]
+
+mergeInf :: Ord a => [[a]] -> [a]
+mergeInf = foldr merge2Sorted []
+    where merge2Sorted (x:xs) ys = x:merge2' xs ys
+          merge2' (x:xs) (y:ys) | x < y  = x:merge2' xs (y:ys)
+                                | x == y = x:merge2' xs ys
+                                | x > y  = y:merge2' (x:xs) ys
