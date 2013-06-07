e0056340979c7e11aec1aaf4b2579bb9b3a765fc
ryanpbrewster <RyanPBrewster@gmail.com>
2013-06-07 16:44:49 -0500

    Sped up the old version of 132.hs, and figured out a much faster way to solve 132.

--- ProjectEuler/Old/132.hs
+++ ProjectEuler/Old/132.hs
@@ -9,35 +9,22 @@
  - Find the sum of the first forty prime factors of R(109).
  -}
 
-import qualified ProjectEuler.Prime as Prime
-
 {-
- - Take advantage of Fermat's little theorem:
- - If p is a prime then
- -     a^(p-1) == 1 (mod p)
- - Let a = 10 and we get
- -     10^(p-1) == 1 (mod p)
- - Given that R(k) = 10^0 + 10^1 + ... + 10^(k-1)
- - we then have a cycle (of length p-1), mod p
- -     R(k) `mod` p = (10^0 `mod` p) + ... + (10^(p-2) `mod` p) + (10^(p-1) `mod` p) + ...
- -                  = 1 + r[1] + ... + r[p-2] + 1 + r[1] + ...
- -                    \--------------------/
- -                        ~k/(p-1) times
+ - We know that A(p) is the smallest value of k such that p | R(k).
+ - Thus, p | R(10^9) iff A(p) | 10^9
+ - That is, if A(p) == k' (such that p | R(k')) then k must be a multiple of k'
  -}
 
--- repunitMod k p == R(k) `mod` p
-repunitMod k p =
-    let rems = iterate (\x -> (10*x) `mod` p) 1
-        rem_cycle = 1 : takeWhile (/= 1) (tail rems)
-        cycle_len = length rem_cycle
-        rem_cycle_sum = (sum rem_cycle) `mod` p
-        num_cycles = (k-1) `quot` cycle_len
-        leftovers = k - num_cycles*cycle_len
-        total = (rem_cycle_sum*num_cycles + (sum $ take leftovers rem_cycle))
-    in total `mod` p
+import qualified ProjectEuler.Prime as Prime
 
-solveProblem count k = let ps = map fromIntegral (drop 3 Prime.primes)
-                           fs = filter (\p -> repunitMod k p == 0) ps
-                       in sum $ take count fs
+-- (a n) stolen from 129.hs. Look there.
+a n = aH n 1 1
+aH n 0 k = k
+aH n r k = let r' = (10*r+1) `mod` n
+           in aH n r' (k+1)
+
+solveProblem count k =
+    let fs = filter (\p -> k `mod` (a p) == 0) (drop 3 Prime.primes)
+    in take count fs
 
 main = print $ solveProblem 40 (10^9)
