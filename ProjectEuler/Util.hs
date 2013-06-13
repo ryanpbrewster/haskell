a13cdcc76d86bfdfc46c6cbe68b6926f85c9865c
ryanpbrewster <RyanPBrewster@gmail.com>
2013-06-13 16:50:39 -0500

    Renamed Util.roll to Util.chunks, and made changes where necessary in the other programs.

--- ProjectEuler/Util.hs
+++ ProjectEuler/Util.hs
@@ -1,21 +1,21 @@
 module ProjectEuler.Util
-( roll
-, rollBy
+( chunks
+, chunksBy
 , tuples
 , sublists
 , mergeInf
 , merge
 ) where
 
--- roll 3 [1..] == [ [1,2,3], [4,5,6], [7,8,9], ... ]
-roll _ [] = []
-roll k xs = let (front,back) = splitAt k xs
-            in front:roll k back
+-- chunks 3 [1..] == [ [1,2,3], [4,5,6], [7,8,9], ... ]
+chunks _ [] = []
+chunks k xs = let (front,back) = splitAt k xs
+              in front:chunks k back
 
--- rollBy generalizes roll in that it allows a list of ks instead of a number
-rollBy _ [] = []
-rollBy (k:ks) xs = let (front,back) = splitAt k xs
-                   in front:rollBy ks back
+-- chunksBy generalizes chunks in that it allows a list of ks instead of a number
+chunksBy _ [] = []
+chunksBy (k:ks) xs = let (front,back) = splitAt k xs
+                     in front:chunksBy ks back
 
 
 -- tuples 2 [1,2,3] == [ [1,1], [1,2], [1,3], [2,1], [2,2], [2,3], [3,1], [3,2], [3,3] ]
