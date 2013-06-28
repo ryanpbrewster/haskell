4c6f114c290020fd0c9bc2c469e905b9bc67e2f6
ryanpbrewster <RyanPBrewster@gmail.com>
2013-06-28 14:05:45 -0500

    Did a little more testing using optimization_test_1.hs. -O3 is pretty much magic.

--- optimization_test_1.hs
+++ optimization_test_1.hs
@@ -1,14 +1,25 @@
 -- optimization_test_1.hs
 
 {- Lessons learned:
- - test4 << test5 << test1 < test2, test3
+ - pretty much anything with an array is fast, if you use optimization
+ - if you do NOT use optimization, then you MUST be careful, as <let>
+ - and <where> constructs can bite you hard
  - 
- - The timings for (ntests, t) == (5000, 5000):
- -     sum1: 1.78
- -     sum2: 3.58
- -     sum3: 3.59
- -     sum4: 0.015
- -     sum5: 0.17
+ - The timings for t == 10000 at -O3
+ -     sum1: {0.520, 0.514, 0.510} -> 0.51
+ -     sum2: {0.014, 0.013, 0.005} -> 0.01
+ -     sum3: {0.010, 0.014, 0.008} -> 0.01
+ -     sum4: {0.008, 0.014, 0.012} -> 0.01
+ -     sum5: {0.234, 0.238, 0.232} -> 0.23
+ -     sum6: {0.015, 0.014, 0.011} -> 0.01
+ -
+ - The timings for t == 10000 at -O0
+ -     sum1: {3.711, 3.744, 3.787} -> 3.75
+ -     sum2: {18.82, 18.91, 19.97} -> 18.9
+ -     sum3: {18.92, 19.67, 19.10} -> 19.0
+ -     sum4: {0.008, 0.022, 0.009} -> 0.01
+ -     sum5: {0.204, 0.203, 0.205} -> 0.20
+ -     sum6: {0.680, 0.688, 0.704} -> 0.69
  -
  - Trying to use arrays and being dumb about it is
  - worse than just being dumb.
@@ -20,8 +31,7 @@
 
 import Data.Array
 
-ntests = 5000
-t = 5000
+t = 10000
 
 perm_memo = listArray (0,t) $ foldList1 (+) [0..t]
 perm_list = foldList1 (+) [0..t]
@@ -33,6 +43,8 @@ sum3 n = let memo = listArray (0,t) $ foldList1 (+) [0..t]
          in memo ! n
 sum4 n = perm_memo ! n
 sum5 n = perm_list !! n
+sum6 n = memo ! n
+    where memo = listArray (0,t) perm_list
 
 foldList1 f (x:xs) = foldList f x xs
 
@@ -40,11 +52,11 @@ foldList _ init [] = [init]
 foldList f init (x:xs) = init:(foldList f (f init x) xs)
 
 
-test1 = print $ maximum [ sum1 t | i <- [1..ntests] ]
-test2 = print $ maximum [ sum2 t | i <- [1..ntests] ]
-test3 = print $ maximum [ sum3 t | i <- [1..ntests] ]
-test4 = print $ maximum [ sum4 t | i <- [1..ntests] ]
-test5 = print $ maximum [ sum5 t | i <- [1..ntests] ]
+test1 = print $ maximum [ sum1 i | i <- [1..t] ]
+test2 = print $ maximum [ sum2 i | i <- [1..t] ]
+test3 = print $ maximum [ sum3 i | i <- [1..t] ]
+test4 = print $ maximum [ sum4 i | i <- [1..t] ]
+test5 = print $ maximum [ sum5 i | i <- [1..t] ]
+test6 = print $ maximum [ sum6 i | i <- [1..t] ]
 
--- main = testslow
-main = test5
+main = test6
