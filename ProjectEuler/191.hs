ec620869b966298e66eb3a9f022acb55ceae6791
ryanpbrewster <RyanPBrewster@gmail.com>
2013-07-09 17:04:54 -0500

    Solved 191.

---
+++ ProjectEuler/191.hs
@@ -0,0 +1,26 @@
+-- 191.hs
+{-
+ - A particular school offers cash rewards to children with good attendance and
+ - punctuality. If they are absent for three consecutive days or late on more
+ - than one occasion then they forfeit their prize.
+ -
+ - During an n-day period a trinary string is formed for each child consisting
+ - of L's (late), O's (on time), and A's (absent).
+ -
+ - Although there are eighty-one trinary strings for a 4-day period that can be
+ - formed, exactly forty-three strings would lead to a prize:
+ -
+ - OOOO OOOA OOOL OOAO OOAA OOAL OOLO OOLA OAOO OAOA
+ - OAOL OAAO OAAL OALO OALA OLOO OLOA OLAO OLAA AOOO
+ - AOOA AOOL AOAO AOAA AOAL AOLO AOLA AAOO AAOA AAOL
+ - AALO AALA ALOO ALOA ALAO ALAA LOOO LOOA LOAO LOAA
+ - LAOO LAOA LAAO
+ -
+ - How many "prize" strings exist over a 30-day period?
+ -}
+
+prize_strings = map sum $ iterate nextDay [1,0,0,0,0,0]
+    where nextDay [a0,a1,a2,la0,la1,la2] = [a0+a1+a2,a0,a1,a0+a1+a2+la0+la1+la2,la0,la1]
+
+main = print solveProblem
+solveProblem = prize_strings !! 30
