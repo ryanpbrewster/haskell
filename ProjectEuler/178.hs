0f573e88cbf91d76b8eb77a7c184ec719e100252
ryanpbrewster <RyanPBrewster@gmail.com>
2013-07-15 15:25:14 -0500

    Reworked 178 to use arrays instead of maps. Slightly faster.

--- ProjectEuler/178.hs
+++ ProjectEuler/178.hs
@@ -17,33 +17,33 @@
 {-
  - Given a set of step numbers which all:
  -     have <n> digits
- -     have lowest digit <lo>
- -     have highest digit <hi>
- -     have leftmost digit <left>
+ -     have lowest digit <l>
+ -     have highest digit <h>
+ -     have leftmost digit <x>
  - we can form a set of <n+1>-digit step numbers with:
- -     left' = left+1, lo' = lo, hi' = max hi (left+1)
- -     left' = left-1, lo' = min lo (left-1), hi' = hi
+ -     x' = x+1, lo' = lo, hi' = max hi (x+1)
+ -     x' = x-1, lo' = min lo (x-1), hi' = hi
  - with the natural caveats that 0 <= left' <= 9
  -
  - We are looking for the set of step numbers with:
- -     <n> = 40
- -     <left> = _
+ -     <n> = 1..40
+ -     <x> = 1..9
  -     <lo> = 0
  -     <hi> = 9
  -}
 
-import Data.Map
+import Data.Array
 
 main = print $ solveProblem 40
 
 solveProblem n = sum [ pandigitalStepNumbers stp | stp <- take n step_numbers ]
 
-pandigitalStepNumbers stp = sum [ findWithDefault 0 (left,0,9) stp | left <- [1..9]]
+pandigitalStepNumbers stp = sum [ stp ! (x,0,9) | x <- [1..9]]
 
-step_numbers = iterate takeOneStep $ fromList [((i,i,i), 1) | i <- [0..9]]
+step_numbers = iterate takeOneStep one_digit
+    where one_digit = accumArray (+) 0 ((0,0,0),(9,9,9)) [((i,i,i), 1) | i <- [0..9]]
 
 takeOneStep stp =
-    let stp' = assocs stp
-        step_up   = [((left+1,lo,max hi (left+1)), count) | ((left,lo,hi), count) <- stp', left < 9 ]
-        step_down = [((left-1,min lo (left-1),hi), count) | ((left,lo,hi), count) <- stp', left > 0 ]
-    in fromListWith (+) $ step_up ++ step_down
+    let step_up   = [((x+1,l,max h (x+1)), stp ! (x,l,h)) | x <- [0..8], l <- [0..x], h <- [x..9]]
+        step_down = [((x-1,min l (x-1),h), stp ! (x,l,h)) | x <- [1..9], l <- [0..x], h <- [x..9]]
+    in accumArray (+) 0 ((0,0,0),(9,9,9)) $ step_up ++ step_down
