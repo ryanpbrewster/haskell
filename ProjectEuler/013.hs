428e341465041fe9390a989fa57628b73a088277
ryanpbrewster <RyanPBrewster@gmail.com>
2013-07-24 14:08:25 -0500

    Changed 013.txt to 013.in

--- ProjectEuler/013.hs
+++ ProjectEuler/013.hs
@@ -1,6 +1,6 @@
 -- 013.hs
 
 
-main = do filetext <- readFile "013.txt"
+main = do filetext <- readFile "013.in"
           let ans = sum $ map read $ lines filetext
           putStrLn $ take 10 $ show ans
