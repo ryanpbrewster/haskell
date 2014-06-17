0f6166e382aadbb170800d2aa98ea42654c94130
ryanpbrewster <RyanPBrewster@gmail.com>
2014-06-17 15:30:09 -0500

    Resolved LCSM in a faster way.

--- Rosalind/LCSM.hs
+++ Rosalind/LCSM.hs
@@ -2,25 +2,32 @@
 {-
  - Problem
  -
- - A common substring of a collection of strings is a substring of every member of the collection. We say that a common substring is a longest common substring if there does not exist a longer common substring. For example, "CG" is a common substring of "ACGTACGT" and "AACCGGTATA", but it is not as long as possible; in this case, "GTA" is a longest common substring of "ACGTACGT" and "AACCGTATA".
+ - A common substring of a collection of strings is a substring of every member
+ - of the collection. We say that a common substring is a longest common
+ - substring if there does not exist a longer common substring. For example,
+ - "CG" is a common substring of "ACGTACGT" and "AACCGGTATA", but it is not as
+ - long as possible; in this case, "GTA" is a longest common substring of
+ - "ACGTACGT" and "AACCGTATA".
  -
- - Note that the longest common substring is not necessarily unique; for a simple example, "AA" and "CC" are both longest common substrings of "AACC" and "CCAA".
+ - Note that the longest common substring is not necessarily unique; for
+ - a simple example, "AA" and "CC" are both longest common substrings of "AACC"
+ - and "CCAA".
  -
  - Given: A collection of k (k≤100) DNA strings of length at most 1 kbp each in FASTA format.
  -
- - Return: A longest common substring of the collection. (If multiple solutions exist, you may return any single solution.)
- - Sample Dataset
+ - Return: A longest common substring of the collection. (If multiple solutions
+ - exist, you may return any single solution.)
  -
- - >Rosalind_1
- - GATTACA
- - >Rosalind_2
- - TAGACCA
- - >Rosalind_3
- - ATACA
+ - Sample Dataset
+ -     >Rosalind_1
+ -     GATTACA
+ -     >Rosalind_2
+ -     TAGACCA
+ -     >Rosalind_3
+ -     ATACA
  -
  - Sample Output
- -
- - AC
+ -     AC
  -}
 import System.Environment (getArgs)
 import qualified Data.Array as A
@@ -37,12 +44,32 @@ main = do
     putStrLn $ solveProblem txt
 
 solveProblem txt = let fastas = parseFASTAs txt
-                       anss = longestCommonSubstrings $ map (getNCLs.getDNA) fastas
+                       ncl_strs = map (getNCLs . getDNA) fastas
+                       anss = longestCommonSubstrings ncl_strs
                    in showDNA $ DNA (head anss)
 
-{- Solution -}
-expandOption opt = [ opt++[a] | a <- alphabet ] ++ [ [a]++opt | a <- alphabet ]
-longestCommonSubstrings xss = lcss [[]]
-    where lcss opts = let maybes = nub $ concat $ map expandOption opts
-                          opts' = filter (\m -> all (m `isInfixOf`) xss) maybes
-                      in if null opts' then opts else lcss opts'
+longestCommonSubstrings (xs:xss) =
+    let f l = null (commonSubstrings l xs xss)
+        len = binarySearch f 0 (length xs + 1)
+    in commonSubstrings len xs xss
+
+binarySearch :: (Int -> Bool) -> Int -> Int -> Int
+binarySearch f lo hi | lo == hi = lo
+                     | otherwise =
+    let mid = (lo + hi + 1) `quot` 2
+    in if f mid then binarySearch f lo (mid-1)
+                else binarySearch f mid hi
+
+commonSubstrings :: Eq a => Int -> [a] -> [[a]] -> [[a]]
+commonSubstrings len source strs =
+    filter (`isCommon` strs) $ sublists len source
+
+isCommon :: Eq a => [a] -> [[a]] -> Bool
+isCommon substr strs = all (substr `isInfixOf`) strs
+
+sublists :: Int -> [a] -> [[a]]
+sublists 0 xs = [[]]
+sublists k xs = let (f,r) = splitAt (k-1) xs in sublists' f r
+    where sublists' sl [] = []
+          sublists' sl (y:ys) = let sl' = sl ++ [y]
+                                in sl' : sublists' (tail sl') ys
