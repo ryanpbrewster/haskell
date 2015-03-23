7db603e8349cfcb5f59b2bd9f61e1955496f475c
ryanpbrewster <RyanPBrewster@gmail.com>
2015-03-23 16:15:06 -0500

    Extended the window min algorithm to window max, and window differentials.

--- window_minmax.hs
+++ window_minmax.hs
@@ -1,4 +1,4 @@
-import Data.Sequence hiding (splitAt)
+import Data.Sequence hiding (splitAt, zipWith)
 
 {-# LANGUAGE ViewPatterns #-}
 
@@ -24,3 +24,23 @@ windowMinHelper w@(viewl -> m :< w') [] _ = [m]
 windowMinHelper w@(viewl -> m :< w') (x:xs) (b:bs)
     | b == m    = m : windowMinHelper (minAdd x w') xs bs
     | otherwise = m : windowMinHelper (minAdd x w)  xs bs
+
+
+
+windowMax winlen xs =
+    let (a,b) = splitAt winlen xs
+        w0 = foldl (flip maxAdd) empty a
+    in windowMaxHelper w0 b xs
+
+-- windowMaxHelper window cur backset
+-- See windowMinHelper for explanation
+windowMaxHelper w@(viewl -> m :< w') [] _ = [m]
+windowMaxHelper w@(viewl -> m :< w') (x:xs) (b:bs)
+    | b == m    = m : windowMaxHelper (maxAdd x w') xs bs
+    | otherwise = m : windowMaxHelper (maxAdd x w)  xs bs
+
+
+differentials winlen xs =
+    let maxs = windowMax winlen xs
+        mins = windowMin winlen xs
+    in zipWith (-) maxs mins
