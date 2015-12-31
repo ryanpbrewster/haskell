bfa221bd44f24d8a0fbd5dd81d5f251119f62f8d
Ryan P Brewster <ryan@kifi.com>
2015-12-31 15:43:38 -0500

    Got it to compile while being simpler.

--- AdventOfCode-2015/07.hs
+++ AdventOfCode-2015/07.hs
@@ -8,15 +8,14 @@ import Data.Maybe (mapMaybe)
 import Control.Monad
 
 main = do
-  nodes <- fmap (map parseNode . lines) (readFile "07.input")
+  nodes <- fmap (M.fromList . map parseNode . lines) (readFile "07.input")
   let a = (solve nodes) M.! "a"
-  let nodes2 = [ n | n@(Node nId _) <- nodes, nId /= NodeId "b" ] ++ [ Node (NodeId "b") (Derived0 $ ConstSource a) ]
-  let a' = (solve nodes2) M.! "a"
   print a
+  let nodes' = M.insert "b" (Const $ Value a) nodes
+  let a' = (solve nodes') M.! "a"
   print a'
   
-
-data Source = Node String | Const Int deriving (Show)
+data Source = Node String | Value Int deriving (Show)
 
 asNode :: Source -> Maybe String
 asNode (Node nId) = Just nId
@@ -30,37 +29,36 @@ data NodeInput = Const Source | Not Source
                deriving (Show)
 
 sources :: NodeInput -> [Source]
-sources (Derived0 s) = [s]
-sources (Derived1 _ s) = [s]
-sources (Derived2 _ a b) = [a, b]
+sources (Const s) = [s]
+sources (Not s) = [s]
+sources (LeftShift a b) = [a, b]
+sources (RightShift a b) = [a, b]
+sources (And a b) = [a, b]
+sources (Or a b) = [a, b]
 
-data Op1 = Not deriving (Show)
-data Op2 = And | Or | LeftShift | RightShift deriving (Show)
-
-data Node = Node NodeId NodeInput deriving (Show)
-
-solve :: [Node] -> M.Map NodeId Int
+solve :: M.Map String NodeInput -> M.Map String Int
 solve ns = solve' ns M.empty
-  where solve' [] acc = acc
-        solve' toBeProcessed acc = 
-          let (canBeProcessed, rest) = partition (evaluable acc) toBeProcessed
-              processed = M.fromList [ (nId, evaluate acc nInp) | Node nId nInp <- canBeProcessed ]
-          in solve' rest (M.union acc processed)
+  where solve' toBeProcessed acc
+          | M.null toBeProcessed = acc
+          | otherwise =
+            let (canBeProcessed, rest) = M.partition (evaluable acc) toBeProcessed
+                processed = M.map (evaluate acc) canBeProcessed
+            in solve' rest (M.union acc processed)
 
 
-evaluable :: M.Map NodeId Int -> Node -> Bool
-evaluable acc (Node _ nInp) =
+evaluable :: M.Map String Int -> NodeInput -> Bool
+evaluable acc nInp =
   all (`M.member` acc) (mapMaybe asNode $ sources nInp)
 
-evaluate :: M.Map NodeId Int -> NodeInput -> Int
-evaluate acc nInp = case nInp of Derived0 s -> eval s
-                                 Derived1 Not s -> complement (eval s)
-                                 Derived2 And a b -> (eval a) .&. (eval b)
-                                 Derived2 Or a b -> (eval a) .|. (eval b)
-                                 Derived2 LeftShift a b -> (eval a) `shiftL` (eval b)
-                                 Derived2 RightShift a b -> (eval a) `shiftR` (eval b)
-  where eval s = case s of ConstSource v -> v
-                           NodeSource nId -> acc M.! nId
+evaluate :: M.Map String Int -> NodeInput -> Int
+evaluate acc nInp = case nInp of Const s -> eval s
+                                 Not s -> complement (eval s)
+                                 And a b -> (eval a) .&. (eval b)
+                                 Or a b -> (eval a) .|. (eval b)
+                                 LeftShift a b -> (eval a) `shiftL` (eval b)
+                                 RightShift a b -> (eval a) `shiftR` (eval b)
+  where eval s = case s of Value v -> v
+                           Node nId -> acc M.! nId
 
 {-
 ######################
@@ -72,58 +70,28 @@ parseNode inp = case parse pNode "" inp of
   Left _ -> error $ "Could not parse: " ++ show inp
   Right v -> v
 
-pNode :: Parser Node
+pNode :: Parser (String, NodeInput)
 pNode = do
   nodeInput <- pNodeInput
   string " -> "
   nodeId <- pNodeId
-  return $ Node nodeId nodeInput
-
-pNodeInput :: Parser NodeInput
-pNodeInput = choice $ map try [ pDerived2, pDerived1, pDerived0 ]
+  return (nodeId, nodeInput)
 
 pSource :: Parser Source
-pSource = choice $ map try [ pInt >>= return . ConstSource
-                           , pNodeId >>= return . NodeSource
-                           ]
-
-pDerived0 :: Parser NodeInput
-pDerived0 = do
-  v <- pSource
-  return $ Derived0 v
-
-pDerived1 :: Parser NodeInput
-pDerived1 = do
-  op <- pOp1
-  spaces
-  s <- pSource
-  return $ Derived1 op s
-
-pDerived2 :: Parser NodeInput
-pDerived2 = do
-  a <- pSource
-  spaces
-  op <- pOp2
-  spaces
-  b <- pSource
-  return $ Derived2 op a b
-
-pOp1 :: Parser Op1
-pOp1 = choice [ string "NOT" >> return Not ]
-
-pOp2 :: Parser Op2
-pOp2 = choice [ string "AND" >> return And
-              , string "OR" >> return Or
-              , string "LSHIFT" >> return LeftShift
-              , string "RSHIFT" >> return RightShift
-              ]
+pSource = choice $ map try [ pInt >>= return . Value , pNodeId >>= return . Node ]
 
 pInt :: Parser Int
-pInt = do
-  v <- many1 digit
-  return $ read v
+pInt = many1 digit >>= return . read
 
-pNodeId :: Parser NodeId
-pNodeId = do
-  nid <- many lower
-  return $ NodeId nid
+pNodeId :: Parser String
+pNodeId = many1 lower >>= return
+
+pNodeInput :: Parser NodeInput
+pNodeInput =
+  choice $ map try [ do string "NOT"; spaces; s <- pSource; return $ Not s
+                   , do a <- pSource; string " AND "; b <- pSource; return $ And a b
+                   , do a <- pSource; string " OR "; b <- pSource; return $ Or a b
+                   , do a <- pSource; string " LSHIFT "; b <- pSource; return $ LeftShift a b
+                   , do a <- pSource; string " RSHIFT "; b <- pSource; return $ RightShift a b
+                   , do s <- pSource; return $ Const s
+                   ]
