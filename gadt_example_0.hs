d126e290382d37f60df51e584dac29e729dbfaff
Ryan P Brewster <ryan@kifi.com>
2016-01-14 23:13:17 -0800

    Fixed GADT example

--- gadt_example_0.hs
+++ gadt_example_0.hs
@@ -1,7 +1,9 @@
+{-# LANGUAGE GADTs #-}
 data Foo a where
   Bar :: Foo a
   Baz :: a -> Foo a -> Foo a
 
 instance Show a => Show (Foo a) where 
   show Bar = ""
-  show (Baz f r) = show f ++ show r
+  show (Baz f Bar) = show f
+  show (Baz f r) = show f ++ " -> " ++ show r
