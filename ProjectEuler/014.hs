96ba0e53e1cdf8ffcf94ffba1c5e5e01083349c3
Ryan P Brewster <ryan@kifi.com>
2016-05-12 00:33:18 -0700

    Fix ProjectEuler makefile and speed up collatz

--- ProjectEuler/014.hs
+++ ProjectEuler/014.hs
@@ -1,17 +1,14 @@
 -- 014.hs
 -- Project Euler problem 14
--- Find the number, k < n, such that the Collatz sequence seeded by k is longest
---
--- Dear lord, please compile this with -O3 or higher. Otherwise it
--- is not even functional.
+-- Find the number, 1 <= k <= n, such that the Collatz sequence seeded by k is longest
 
+main = print $ problem014 (10^6)
 
-collatzLength n = cl n 0
+problem014 :: Int -> Int
+problem014 n = snd $ maximum $ [ (collatz x, x) | x <- [1..n] ]
 
-cl 1 c = c+1
-cl n c | even n = cl (n `quot` 2) (c+1)
-       | odd n  = cl (3*n+1)      (c+1)
+collatz :: Int -> Int
+collatz n = length $ takeWhile (>1) $ iterate next n
 
-problem014 n = snd $ maximum $ [ (collatzLength x, x) | x <- [1..n] ]
-
-main = print ans where ans = problem014 (10^6)
+next :: Int -> Int
+next n = if even n then n `div` 2 else 3*n+1
