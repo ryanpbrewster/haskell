bdd939ee8b7cf9d5d45c969a757e67f24998dc5b
Ryan Brewster <ryanpbrewster@gmail.com>
2016-12-31 12:46:58 -0800

    Massive restructing because I hate loose files

---
+++ poker/src/Data.hs
@@ -0,0 +1,16 @@
+module Poker.Data where
+
+data Rank = Two
+          | Three
+          | Four
+          | Five
+          | Six
+          | Seven
+          | Eight
+          | Nine
+          | Ten
+          | Jack
+          | Queen
+          | King
+          | Ace
+          deriving (Eq, Ord, Show)
