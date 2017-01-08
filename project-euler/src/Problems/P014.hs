39cda18b78f69a2828725361227541f256efd1ab
Ryan Brewster <ryanpbrewster@gmail.com>
2017-01-08 00:12:22 -0800

    Alphabetize functions in Util.List package

--- project-euler/src/Problems/P014.hs
+++ project-euler/src/Problems/P014.hs
@@ -1,14 +1,16 @@
 module Problems.P014 (solve) where
-{- 014.hs
+{-
  - Project Euler problem 14
  - Find the number, 1 <= k <= n, such that the Collatz sequence seeded by k is longest
  -}
+
+import Util.List (maximumBy)
 
 solve :: String
 solve = show $ slowSolve 1e6
 
 slowSolve :: Int -> Int
-slowSolve n = snd $ maximum $ [ (collatz x, x) | x <- [1..n] ]
+slowSolve n = maximumBy collatz [1..n]
 
 collatz :: Int -> Int
 collatz 1 = 0
--- project-euler/src/Util/List.hs
+++ project-euler/src/Util/List.hs
@@ -1,12 +1,15 @@
 module Util.List
 ( chunks
 , chunksBy
-, tuples
+, maximumBy
+, merge
+, mergeInf
 , ordTuples
 , sublists
-, mergeInf
-, merge
+, tuples
 ) where
+
+import Data.List (foldl')
 
 -- chunks 3 [1..] == [ [1,2,3], [4,5,6], [7,8,9], ... ]
 chunks _ [] = []
@@ -17,11 +20,21 @@ chunks k xs = let (front,back) = splitAt k xs
 chunksBy _ [] = []
 chunksBy (k:ks) xs = let (front,back) = splitAt k xs
                      in front:chunksBy ks back
+maximumBy :: Ord b => (a -> b) -> [a] -> a
+maximumBy keyFn (a:as) = fst $ foldl' max' (a, keyFn a) as
+  where
+  max' (x, kx) y =
+    let ky = keyFn y in if kx < ky then (y, ky) else (x, kx)
 
+merge xs [] = xs
+merge [] ys = ys
+merge (x:xs) (y:ys) | x < y  = x:merge xs (y:ys)
+                  | x == y = x:y:merge xs ys
+                  | x > y  = y:merge (x:xs) ys
 
--- tuples 2 [1,2,3] == [ [1,1], [1,2], [1,3], [2,1], [2,2], [2,3], [3,1], [3,2], [3,3] ]
-tuples 0 _ = [[]] -- only the empty tuple
-tuples k xs = [ x:t | t <- tuples (k-1) xs, x <- xs ]
+mergeInf :: Ord a => [[a]] -> [a]
+mergeInf = foldr merge2Sorted []
+    where merge2Sorted (x:xs) ys = x:merge xs ys
 
 -- ordTuples yields all the k-tuples where all adjacent elements in the tuple
 -- obey the predicate. For example, if pred == (<) then all tuples will be in
@@ -32,18 +45,14 @@ ordTuples pred k xs = [ x:t | t <- ordTuples pred (k-1) xs
                             , x <- xs
                             , x `pred` (head t) ]
 
+
 -- similar to tuples, but maintains order
 -- `sublists 2` will retrieve all distinct pairs from a list
 sublists 0 _ = [[]]
 sublists _ [] = []
 sublists k (x:xs) = [ x:l | l <- sublists (k-1) xs ] ++ sublists k xs
 
-mergeInf :: Ord a => [[a]] -> [a]
-mergeInf = foldr merge2Sorted []
-    where merge2Sorted (x:xs) ys = x:merge xs ys
+-- tuples 2 [1,2,3] == [ [1,1], [1,2], [1,3], [2,1], [2,2], [2,3], [3,1], [3,2], [3,3] ]
+tuples 0 _ = [[]] -- only the empty tuple
+tuples k xs = [ x:t | t <- tuples (k-1) xs, x <- xs ]
 
-merge xs [] = xs
-merge [] ys = ys
-merge (x:xs) (y:ys) | x < y  = x:merge xs (y:ys)
-                  | x == y = x:y:merge xs ys
-                  | x > y  = y:merge (x:xs) ys
