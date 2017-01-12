2e93274876e86367f09832a6538fa580604a9840
Ryan Brewster <ryanpbrewster@gmail.com>
2017-01-11 23:07:07 -0800

    Optimized P014 by using an array to memoize intermediate results

--- project-euler/src/Problems/P014.hs
+++ project-euler/src/Problems/P014.hs
@@ -6,16 +6,25 @@ module Problems.P014
  - Project Euler problem 14
  - Find the number, 1 <= k <= n, such that the Collatz sequence seeded by k is longest
  -}
+import Data.Array
 import Util.List (maximumBy)
 
 solve :: String
-solve = show $ slowSolve 1e6
+solve = show $ fastSolve 1e6
 
 slowSolve :: Int -> Int
-slowSolve n = maximumBy collatz [1 .. n]
+slowSolve bound = maximumBy collatz [1 .. bound]
+
+fastSolve :: Int -> Int
+fastSolve bound = maximumBy (memo !) [1 .. bound]
+  where
+  memo = listArray (1, bound) (1 : map f [2..bound])
+  f n =
+    let n' = next n
+    in 1 + if n' <= bound then memo ! n' else f n'
 
 collatz :: Int -> Int
-collatz 1 = 0
+collatz 1 = 1
 collatz n = 1 + collatz (next n)
 
 next :: Int -> Int
