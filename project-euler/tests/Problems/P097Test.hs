25b54c02983000e5ffb583a9b3133bbfc8d300c7
Ryan Brewster <ryanpbrewster@gmail.com>
2017-01-12 17:52:39 -0800

    Fix P097 and P099 tests

--- project-euler/tests/Problems/P097Test.hs
+++ project-euler/tests/Problems/P097Test.hs
@@ -6,4 +6,4 @@ import Problems.P097
 import Test.Tasty.Discover (Assertion, (@?=))
 
 case_097_main :: Assertion
-case_097_main = solve @?= "233168"
+case_097_main = solve @?= "8739992577"
--- project-euler/tests/Problems/P099Test.hs
+++ project-euler/tests/Problems/P099Test.hs
@@ -3,7 +3,8 @@ module Problems.P099Test
   ) where
 
 import Problems.P099
+import qualified TestData.P099
 import Test.Tasty.Discover (Assertion, (@?=))
 
 case_099_main :: Assertion
-case_099_main = process "foo" @?= "233168"
+case_099_main = process TestData.P099.txt @?= "709"
