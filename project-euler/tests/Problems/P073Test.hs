de960c2f0c5f8f333f6b2b02136a4ddfa824289b
Ryan Brewster <ryanpbrewster@gmail.com>
2017-01-14 14:56:53 -0800

    Solve P073, add a documentation directory

---
+++ project-euler/tests/Problems/P073Test.hs
@@ -0,0 +1,11 @@
+module Problems.P073Test
+  ( test_073
+  ) where
+
+import Problems.P073
+import Test.Tasty.Discover
+
+test_073 :: [TestTree]
+test_073 =
+  [ testCase "main" $ solve @?= "7295372"
+  ]
