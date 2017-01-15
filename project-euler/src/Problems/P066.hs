4c7c2d43c0e1c266bee496bf4ac358dfa9d9e3ca
Ryan Brewster <ryanpbrewster@gmail.com>
2017-01-15 01:48:49 -0800

    Use Farey sequences to solve P066

--- project-euler/src/Problems/P066.hs
+++ project-euler/src/Problems/P066.hs
@@ -3,6 +3,8 @@ module Problems.P066
   , solveProblem
   ) where
 
+import Data.List (find)
+import Data.Maybe (fromJust)
 {-
  - Consider quadratic Diophantine equations of the form:
  -
@@ -28,53 +30,34 @@ module Problems.P066
  - Find the value of D ≤ 1000 in minimal solutions of x for which the largest
  - value of x is obtained.
  -}
-
-import Debug.Trace
 import Util.List (maximumBy)
-import Data.Ratio
-import Data.List (find, foldl1, inits, unfoldr)
-import Data.Maybe (fromJust)
 
 solve = show $ solveProblem 1000
 
 solveProblem :: Integer -> Integer
-solveProblem bound = maximumBy (fst . minimalSolution) ([1..bound] `minus` squares)
+solveProblem bound =
+  maximumBy (fst . minimalSolution) ([1 .. bound] `minus` squares)
 
 minimalSolution :: Integer -> (Integer, Integer)
 minimalSolution d =
-  let cfrac = sqrtContinuedFraction d
-      candidates = [ (numerator v, denominator v) | v <- map fromContinuedFraction $ tail $ inits cfrac ]
-  in fromJust $ find (\(x,y) -> x*x - d*y*y == 1) candidates
+  fromJust $
+  find (\(x, y) -> y > 0 && x * x - d * y * y == 1) (sqrtConvergents d)
 
-squares = map (^2) [1..]
+sqrtConvergents :: Integer -> [(Integer, Integer)]
+sqrtConvergents n = explore (0, 1) (1, 0)
+  where
+    explore (a, b) (e, f) =
+      let (c, d) = (a + e, b + f)
+      in (c, d) :
+         if c * c < n * d * d
+           then explore (c, d) (e, f)
+           else explore (a, b) (c, d)
+
+squares = map (^ 2) [1 ..]
 
 minus xs [] = xs
 minus [] _ = []
 minus (x:xs) (y:ys)
-  | x < y  = x : minus xs (y:ys)
-  | x > y  = minus (x:xs) ys
+  | x < y = x : minus xs (y : ys)
+  | x > y = minus (x : xs) ys
   | x == y = minus xs ys
-
-
-fromContinuedFraction :: [Integer] -> Ratio Integer
-fromContinuedFraction = foldr1 (\a b -> a + 1 / b) . map (%1)
-
--- sqrt(n) = 
-sqrtContinuedFraction :: Integer -> [Integer]
-sqrtContinuedFraction n =
-  unfoldr (Just . nextFraction) (1, 0, 1)
-  where
-  x = sqrt (fromIntegral n)
-  -- (b*sqrt(n) + c) / d
-  --  --> 1 / ( (b*sqrt(n) + c)/d - a )
-  --   == d / ( b*sqrt(n) + c - a*d ) [ let e = c - a*d ]
-  --   == d * (b*sqrt(n) - e) / (b*sqrt(n) + e) * (b*sqrt(n) - e)
-  --   == (d*b * sqrt(n) - d*e) / (b*b*n - e*e)
-  nextFraction (b, c, d) =
-    let a = floor $ (x * fromIntegral b + fromIntegral c) / fromIntegral d
-        e = c - a * d
-        b' = d * b
-        c' = -d * e
-        d' = b * b * n - e * e
-        g = foldl1 gcd [b', c', d']
-    in (a, (b' `div` g, c' `div` g, d' `div` g))
